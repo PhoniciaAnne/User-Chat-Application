7e9e7cb164614dc2b011976a5bdb002313da11cc
Phonicia Anne Peramaiyan <153049020+PhoniciaAnne@users.noreply.github.com>
2024-06-01 16:25:26 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!-- Coding By PhoniciaAnne  -->
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title> Realtime chat app| PhoniciaAnne</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
